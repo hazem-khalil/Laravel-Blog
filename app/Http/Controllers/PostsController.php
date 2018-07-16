667a6dab2517413ebd174f21f76d30236526f8c1
hazem-khalil <hazem.ibrahim.khalil@gmail.com>
2018-07-16 02:06:42 +0200

    Rendering Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,12 +10,16 @@ class PostsController extends Controller
 {
     public function index()
     {
-    	return view('posts.index');
+    	$posts = Post::orderBy('created_at', 'desc')->get();
+
+    	return view('posts.index', compact('posts'));
     }
 
-    public function show()
+    public function show($id)
     {
-    	return view('posts.show');
+    	$post = Post::find($id);
+
+    	return view('posts.show', compact('post'));
     }
 
     public function create()
