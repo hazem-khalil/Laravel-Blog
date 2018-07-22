e8ab7babf1dc75d4a6acb3787a6b33ac9911cf8b
= <=>
2018-07-22 15:20:17 +0200

    Add Comments to Posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,5 +7,48 @@
 
 		{{$post->body}}
 
+		<hr>
+
+		<div class="comments">
+			<ul class="list-group">
+				@foreach ($post->comments as $comment)
+					<li class="list-group-item">	
+						<strong>
+							{{ $comment->created_at->diffForHumans() }}:&nbsp
+						</strong>
+
+						{{ $comment->body }}
+					</li>
+				@endforeach
+			</ul>
+		</div>
+
+		<hr>
+
+		<div class="card">
+			<div class="card-block">
+				
+				<form method="POST" action="/posts/{{ $post->id }}/comments">
+
+					{{ csrf_field() }}
+					
+					<div class="form-group">
+						<textarea name="body" placeholder="Your comment here!" class="form-control" required></textarea>
+					</div>
+
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary">Add Commit</button>
+				    </div>
+
+
+				    @include ('layouts.errors')
+
+				</form>
+
+			</div>
+		</div>
+
 	</div>
+		
+
 @endsection
